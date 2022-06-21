dd004e3a6c7048f6595fcd51128694ced1b1775f
Francis Brokering <francisbrokering@gmail.com>
2022-06-21 09:52:44 +0900

    my first React project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useEffect, useState } from 'react'
+
+const App = () => {
+
+    const [userChoice, setuserChoice] = useState(null)
+    const [computerChoice, setComputerChoice] = useState(null)
+    const [result, setResult] = useState(null)
+    const choices = ['rock', 'paper', 'scissors']
+
+
+    const handleClick = (value) => {
+        setuserChoice(value)
+        generateComputerChoice()
+        // checkResult()
+    }
+
+    const generateComputerChoice = () => {
+        const randomChoice = choices[Math.floor(Math.random() * choices.length)]
+        setComputerChoice(randomChoice)
+    }
+
+    useEffect(()=>{
+        checkResult()
+    },  [computerChoice, userChoice])
+    const checkResult = () => {
+        switch(userChoice + computerChoice){
+            case 'scissorspaper':
+            case 'rockscissors':
+            case 'paperrock':
+                setResult('YOU WIN!')
+                break
+            case 'paperscissors':
+            case 'scissorsrock':
+            case 'rockpaper':
+                setResult('YOU LOSE!')
+                break
+            case 'rockrock':
+            case 'paperpaper':
+            case 'scissorsscissors':
+                setResult('ITS A DRAW!')
+
+        }
+    }
+
+    return ( 
+        <div>
+            <h1>user choise is: {userChoice}</h1>
+            <h1>computer choise is: {computerChoice} </h1>
+            {choices.map((choice, index) => <button key={index} onClick={()=>handleClick(choice)}>{choice}</button>)}
+            <h1>{result}</h1>
+        </div>
+    );
 }
 
-export default App;
+export default App
